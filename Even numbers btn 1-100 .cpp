523e34ab0f62899a6e6ea69355a7c3aab06dab74
novekipchirchir <114153153+novekipchirchir@users.noreply.github.com>
2022-10-06 08:39:00 +0300

    Add files via upload

---
+++ Even numbers btn 1-100 .cpp
@@ -0,0 +1,12 @@
+//Printing even numbers between 1 and 100
+#include <iostream>
+using namespace std;
+int main(){
+	int a;
+	for(a=1; a<=100; a++){
+		if(a%2==0){
+			cout<<a<<endl;
+		}
+	}
+	return 0;
+}
